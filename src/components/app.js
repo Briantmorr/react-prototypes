36d27d91043e8f0fad71c69a6e1ab772d555ee4a
briantmorr <briantmorr@gmail.com>
2018-03-18 23:42:24 -0700

    finished basic stopwatch

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,8 @@
-
+import React from "react";
+import Stopwatch from './stopwatch';
+export default () => (
+    <div className="container">
+         <h1 className="text-center"> Stopwatch </h1>
+         <Stopwatch/>  
+    </div>)
 
